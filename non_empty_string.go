05b11713c0b7f359be815dfe874cfd765df6a593
Maxim Motyshen <m.motyshen@gmail.com>
2024-10-12 18:16:05 +0300

    update

--- non_empty_string.go
+++ non_empty_string.go
@@ -2,16 +2,19 @@ package invarcol
 
 import invar "github.com/m-ocean-it/GoInvar"
 
-var nonEmptyStringConditions = []invar.Condition[string]{
-	func(x string) bool { return len(x) > 0 },
+var nonEmptyStringInvariants = []invar.Invariant[string]{
+	{
+		Name:  "string must be non-empty",
+		Check: func(x string) bool { return len(x) > 0 },
+	},
 }
 
-type NonEmptyString invar.Invariant[string]
+type NonEmptyString invar.InvariantsHolder[string]
 
 func NewNonEmptyString(s string) NonEmptyString {
-	return invar.New(s, nonEmptyStringConditions)
+	return invar.New(s, nonEmptyStringInvariants)
 }
 
 func TryNewNonEmptyString(s string) (NonEmptyString, error) {
-	return invar.TryNew(s, nonEmptyStringConditions)
+	return invar.TryNew(s, nonEmptyStringInvariants)
 }
--- positive_int.go
+++ positive_int.go
@@ -2,16 +2,19 @@ package invarcol
 
 import invar "github.com/m-ocean-it/GoInvar"
 
-var positiveIntConditions = []invar.Condition[int]{
-	func(x int) bool { return x > 0 },
+var positiveIntInvariants = []invar.Invariant[int]{
+	{
+		Name:  "number must be positive",
+		Check: func(x int) bool { return x > 0 },
+	},
 }
 
-type PositiveInt invar.Invariant[int]
+type PositiveInt invar.InvariantsHolder[int]
 
 func NewPositiveInt(n int) PositiveInt {
-	return invar.New(n, positiveIntConditions)
+	return invar.New(n, positiveIntInvariants)
 }
 
 func TryNewPositiveInt(n int) (PositiveInt, error) {
-	return invar.TryNew(n, positiveIntConditions)
+	return invar.TryNew(n, positiveIntInvariants)
 }
